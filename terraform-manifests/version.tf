4c1c314c7e4f41985a42756ec618b01dbec97d2a
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-02-08 00:05:47 +0800

    feat: Remote Backend  Ref: #14

--- terraform-manifests/version.tf
+++ terraform-manifests/version.tf
@@ -7,6 +7,16 @@ terraform {
     }
   }
 
+  # Remote Backend
+  backend "s3" {
+    bucket         = "tfstate-dev-us-east-1-jpjtof"
+    key            = "vpc/dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }   
+}
+
 provider "aws" {
   region = var.aws_region
 }
