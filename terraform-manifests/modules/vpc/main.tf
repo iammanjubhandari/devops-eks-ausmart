c0e98df868ac1912c9e6c18535e4157fd07447c5
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-02-08 01:08:39 +0800

    feat: add Route Table Association  Ref: #18

--- terraform-manifests/modules/vpc/main.tf
+++ terraform-manifests/modules/vpc/main.tf
@@ -34,3 +34,44 @@ resource "aws_subnet" "private" {
     Name = "${var.environment_name}-private-${each.key}"
   })
 }
+
+resource "aws_eip" "nat" {
+  tags = merge(var.tags, { Name = "${var.environment_name}-nat-eip" })
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public)[0].id
+  tags = merge(var.tags, { Name = "${var.environment_name}-nat" })
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-public-rt" })
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  for_each = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = merge(var.tags, { Name = "${var.environment_name}-private-rt" })
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  for_each = aws_subnet.private
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt.id
+}
