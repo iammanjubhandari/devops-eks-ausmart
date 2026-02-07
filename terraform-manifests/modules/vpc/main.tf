d0b0f82d4bee237145156542d42e45e3d8b66892
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-02-08 00:42:30 +0800

    feat: create private subnets for internal services Ref: #18

--- terraform-manifests/modules/vpc/main.tf
+++ terraform-manifests/modules/vpc/main.tf
@@ -12,3 +12,16 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = merge(var.tags, { Name = "${var.environment_name}-igw" })
 }
+
+resource "aws_subnet" "public" {
+  for_each = { for idx, az in local.azs : az => local.public_subnets[idx] }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = each.value
+  availability_zone       = each.key
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name = "${var.environment_name}-public-${each.key}"
+  })
+}
+
