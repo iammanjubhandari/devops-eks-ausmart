eb7c9f4d2b89581d00c41765e9b2c306bc5f7c99
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-02-07 22:53:58 +0800

    feat: create S3 bucket and DynamoDB table for Terraform backend

--- remote_backend_s3bucket/s3bucket.tf
+++ remote_backend_s3bucket/s3bucket.tf
@@ -33,4 +33,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_encryptio
   }
 }
 
-
+resource "aws_s3_bucket_public_access_block" "tfstate_block_public" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
