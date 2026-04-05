c3bf67e9c860edd7fa2d99ba4c23e26e7e629a45
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 13:01:27 +0800

    wip: add policy and pod identity - still need the addon resource

--- modules/eks-addons/c5-ebs-csi.tf
+++ modules/eks-addons/c5-ebs-csi.tf
@@ -13,3 +13,17 @@ resource "aws_iam_role" "ebs_csi" {
 
   tags = var.common_tags
 }
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi.name
+}
+
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi.arn
+
+  tags = var.common_tags
+}
