d2db011673ab7d7955c323670ce67c60372b3e8f
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 10:53:00 +0800

    feat: add managed node group with AL2023 in private subnets Ref: #40

--- modules/eks-cluster/c5-eks-nodegroup.tf
+++ modules/eks-cluster/c5-eks-nodegroup.tf
@@ -26,3 +26,40 @@ resource "aws_launch_template" "eks_nodes" {
 
   tags = var.common_tags
 }
+
+# managed node group - private subnets, AL2023, on-demand
+resource "aws_eks_node_group" "private" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.name_prefix}-private-nodes"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = var.private_app_subnet_ids
+
+  ami_type       = "AL2023_x86_64_STANDARD"
+  capacity_type  = "ON_DEMAND"
+  instance_types = [var.node_instance_type]
+
+  launch_template {
+    id      = aws_launch_template.eks_nodes.id
+    version = aws_launch_template.eks_nodes.latest_version
+  }
+
+  scaling_config {
+    min_size     = var.node_min_size
+    max_size     = var.node_max_size
+    desired_size = var.node_desired_size
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "${var.name_prefix}-private-nodes"
+  })
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_readonly
+  ]
+}
