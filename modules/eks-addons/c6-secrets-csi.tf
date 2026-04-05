580707696cc58d9b03d7a1be7199d327fe662263
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 17:47:42 +0800

    wip: enable secret sync and rotation - not sure if rotation works yet

--- modules/eks-addons/c6-secrets-csi.tf
+++ modules/eks-addons/c6-secrets-csi.tf
@@ -6,5 +6,16 @@ resource "helm_release" "secrets_store_csi" {
   namespace  = "kube-system"
   version    = "1.4.1"
 
+  # need syncSecret so CSI creates actual k8s secrets from SM values
+  set {
+    name  = "syncSecret.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "enableSecretRotation"
+    value = "true"
+  }
+
   depends_on = [aws_eks_addon.pod_identity]
 }
