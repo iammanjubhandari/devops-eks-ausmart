a25f5cacd8e1f72d388051954b87b86cfabcd3c8
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 12:49:28 +0800

    feat: IAM role and pod identity for LBC ref: #46

--- modules/eks-addons/c4-lbc.tf
+++ modules/eks-addons/c4-lbc.tf
@@ -141,3 +141,33 @@ resource "aws_iam_policy" "lbc" {
 
   tags = var.common_tags
 }
+
+resource "aws_iam_role" "lbc" {
+  name = "${var.name_prefix}-lbc-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "pods.eks.amazonaws.com" }
+      Action    = ["sts:AssumeRole", "sts:TagSession"]
+    }]
+  })
+
+  tags = var.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "lbc" {
+  policy_arn = aws_iam_policy.lbc.arn
+  role       = aws_iam_role.lbc.name
+}
+
+# pod identity so LBC pods can assume the role
+resource "aws_eks_pod_identity_association" "lbc" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+  role_arn        = aws_iam_role.lbc.arn
+
+  tags = var.common_tags
+}
