aea05788ac64508f45329e3448ad7e94ce434408
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 11:48:32 +0800

    feat: add kubernetes provider with exec-based auth Ref: #42

--- envs/au-dev/c1-versions.tf
+++ envs/au-dev/c1-versions.tf
@@ -42,3 +42,14 @@ provider "helm" {
     }
   }
 }
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+  }
+}
