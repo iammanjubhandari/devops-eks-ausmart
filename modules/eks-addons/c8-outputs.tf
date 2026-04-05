a1f325131e3caed15b7ba5a5c4e518b5ec72c315
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 13:03:22 +0800

    add ebs csi output

--- modules/eks-addons/c8-outputs.tf
+++ modules/eks-addons/c8-outputs.tf
@@ -2,3 +2,7 @@ output "lbc_role_arn" {
   description = "IAM role ARN for Load Balancer Controller"
   value       = aws_iam_role.lbc.arn
 }
+
+output "ebs_csi_role_arn" {
+  value = aws_iam_role.ebs_csi.arn
+}
