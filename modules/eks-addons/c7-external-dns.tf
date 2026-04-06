044e270a5d48583f01a7de2c6a32c69581bf1e19
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-06 11:16:06 +0800

    feat: route53 policy and pod identity for external-dns

--- modules/eks-addons/c7-external-dns.tf
+++ modules/eks-addons/c7-external-dns.tf
@@ -13,3 +13,37 @@ resource "aws_iam_role" "external_dns" {
 
   tags = var.common_tags
 }
+
+resource "aws_iam_role_policy" "external_dns" {
+  name = "${var.name_prefix}-external-dns"
+  role = aws_iam_role.external_dns.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["route53:ChangeResourceRecordSets"]
+        Resource = ["arn:aws:route53:::hostedzone/*"]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "route53:ListHostedZones",
+          "route53:ListResourceRecordSets",
+          "route53:ListTagsForResource"
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+}
+
+resource "aws_eks_pod_identity_association" "external_dns" {
+  cluster_name    = var.cluster_name
+  namespace       = "kube-system"
+  service_account = "external-dns"
+  role_arn        = aws_iam_role.external_dns.arn
+
+  tags = var.common_tags
+}
